51eb776b4e0ac38eab3a64566abf8923e56b3e3a
Marta Markiv <mpat@keplercode.com>
2024-05-22 14:45:01 +0300

    Fixed bug with configuration

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   countPerPage: 10,
-  dbUrl: "mongodb+srv://markivmartab:AUcY1G4QzetYRhBq@eventsregistration.d8azsch.mongodb.net/apidb"//process.env.MONGO_URL || "mongodb://localhost:27017/apidb"
+  dbUrl: process.env.MONGO_URL || "mongodb://localhost:27017/apidb"
 };
